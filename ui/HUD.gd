29c327d541d8b9e58d52dc6dec032a7e00ce59d3
Stewart Chan <stew.chan@gmail.com>
2022-11-17 17:47:14 -0500

    Add flash leaderboard

--- ui/HUD.gd
+++ ui/HUD.gd
@@ -8,14 +8,28 @@ const HIGHSCORE_MAX_RESULTS = 10
 const MAX_NAME_LENGTH = 12
 
 
+onready var message_label = $MessageLabel
+onready var leaderboard = $Leaderboard
+onready var tween_out = $TweenOut
+onready var tween_in = $TweenIn
+
+
 func _ready():
 	$NameLabel.text = Global.player_name
-
+	leaderboard.render_board(SilentWolf.Scores.scores, [])
+	
+	tween_out.connect("tween_all_completed", self, "tween_out_completed")
+	tween_in.connect("tween_all_completed", self, "tween_in_completed")
+	
+	yield(get_tree().create_timer(2),"timeout")
+	
+	
+	tween_in.start()
 
 # Show message with a delay
 func show_message(text):
-	$MessageLabel.text = text
-	$MessageLabel.show()
+	message_label.text = text
+	message_label.show()
 	$MessageTimer.start()
 
 
@@ -27,9 +41,9 @@ func show_game_over(score):
 	yield(SilentWolf.Scores.get_high_scores(HIGHSCORE_MAX_RESULTS), "sw_scores_received")
 
 	$ScoreLabel.hide()
-	$Leaderboard.clear_leaderboard()
-	$Leaderboard.render_board(SilentWolf.Scores.scores, [])
-	$Leaderboard.show()
+	leaderboard.clear_leaderboard()
+	leaderboard.render_board(SilentWolf.Scores.scores, [])
+	leaderboard.show()
 	yield(get_tree().create_timer(2), "timeout")
 	# $SettingsButton.show()
 	$StartButton.show()
@@ -44,13 +58,13 @@ func _on_StartButton_pressed():
 	$NameLabel.hide()
 	$StartButton.hide()
 	# $SettingsButton.hide()
-	$Leaderboard.hide()
+	leaderboard.hide()
 	$ScoreLabel.show()
 	emit_signal("start_game")
 
 
 func _on_MessageTimer_timeout():
-	$MessageLabel.hide()
+	message_label.hide()
 
 
 # func _on_SettingsButton_pressed():
@@ -61,7 +75,7 @@ func _on_MessageTimer_timeout():
 func _on_NameLabelButton_pressed():
 	if OS.has_feature("JavaScript"):
 		var name = JavaScript.eval(
-			"""
+		"""
 			prompt("Player Name:", name)
 		"""
 		)
@@ -72,3 +86,19 @@ func _on_NameLabelButton_pressed():
 			name = name.substr(0, MAX_NAME_LENGTH)
 			$NameLabel.text = name
 			Global.save_name(name)
+
+
+	
+func tween_out_completed():
+	tween_in.interpolate_property(message_label, "modulate:a", 0, 1, 1.5)
+	tween_in.interpolate_property(leaderboard, "modulate:a", 1, 0, 1.5)
+	yield(get_tree().create_timer(5),"timeout")
+	tween_in.start()
+
+
+func tween_in_completed():
+	tween_out.interpolate_property(message_label, "modulate:a", 1, 0, 1.5)
+	tween_out.interpolate_property(leaderboard, "modulate:a", 0, 1, 1.5)
+	yield(get_tree().create_timer(5),"timeout")
+	tween_out.start()
+	
